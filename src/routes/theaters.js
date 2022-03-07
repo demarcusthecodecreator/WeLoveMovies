7edc787da25232c9d74b7e52ad6b443f5dddc180
demarcusthecodecreator <90996974+demarcusthecodecreator@users.noreply.github.com>
2022-03-07 12:45:34 -0600

    Delete src directory

--- src/routes/theaters.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const theaters = require('../controllers/theater')
-
-const router = express.Router();
-
-router.route('/').get(theaters.getAllTheaters);
-
-module.exports = router;
